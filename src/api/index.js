195b820e9083aefa600d77096d7f6a01fa6afb3c
abdulmalik-devs <malexmazzy@gmail.com>
2023-05-25 18:20:52 +0100

    new

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const url = '54.197.75.161';
+
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
